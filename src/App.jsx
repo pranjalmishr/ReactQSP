92ce274662aede43f92ab43f64f10908bc5e3ec9
Pranjal Mishra <mishrapranjal232@gmail.com>
2024-11-27 16:21:08 +0530

    React Fist Day

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./Components/navbar";
+const App=()=>{
+    return(
+        <div>
+            <Navbar />
+            <h1>Yhi wali lunga, yhi wali lunga, nhi manunga, nhi manunga</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Components/navbar.jsx
@@ -0,0 +1,14 @@
+
+const Navbar= ()=>{
+    return(
+        <nav>
+            <ul>
+               <li>Home</li>
+                <li>about</li>
+                <li>login</li>
+                <li>register</li>
+            </ul>
+        </nav>
+    );
+};
+export default Navbar;
